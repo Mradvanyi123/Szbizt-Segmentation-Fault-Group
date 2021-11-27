734bf808727d8f1a28245df1303e9307a2d22511
Stella Tóth-Baranyi <stellasipi@gmail.com>
2021-11-28 00:01:42 +0100

    Add empty auth server

---
+++ caffauthserver/src/main/java/hu/bme/sfg/caffauthserver/CaffauthserverApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.sfg.caffauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffauthserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaffauthserverApplication.class, args);
+    }
+
+}
