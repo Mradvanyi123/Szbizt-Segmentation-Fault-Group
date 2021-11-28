6db45a8c99378cfaedc94c27e3b1aec66d43ec3f
Stella Tóth-Baranyi <stellasipi@gmail.com>
2021-11-28 17:13:18 +0100

    Add updated database setup

--- catalogbackend/README.md
+++ catalogbackend/README.md
@@ -1,3 +1,4 @@
+
 # CAFF Catalog Backend
 
 ## Setup
@@ -6,12 +7,27 @@
 - Maven
 - Java 11
 
-**Adatbázis**
+## Használat
+***1. H2 in-memory adatbázissal***
+
 Mivel lokális H2 adatbázist használunk, így nincs szükség semmilyen setup-ra. Elérni a futtatás indítása után a http://localhost:8080/api/h2-console url-en lehet elérni.
-*A JDBC url, username és a password elérhetőek az application.properties fájlban.*
+*A JDBC url, username és a password elérhetőek az application-h2.properties fájlban.*
 
+**Fordítás**
 
-## Futtatás
+    mvn clean install -Ph2
+
+**Futtatás**
+
+    mvn spring-boot:run -Dspring-boot.run.profiles=h2
+
+***2. PostgreSQL adatbázissal***
+
+**Előkövetelmények**
+- PostgreSQL
+
+Először is  a PostgreSQL telepítése után létre kell hozni egy *caffdb* nevű adatbázist, illetve egy *caff_user* user-t az application.properties fájlban leírt hozzátartozó jelszóval. Ezután elérhető az adatbázis és futtathatók a következők (már ha üres az összes tábla):
+
 **Fordítás**
 
     mvn clean install
@@ -19,4 +35,6 @@ Mivel lokális H2 adatbázist használunk, így nincs szükség semmilyen setup-
 **Futtatás**
 
     mvn spring-boot:run
-Ezután a végpontok elérhetőek a http://localhost:8080/api/ url-en.
+
+## FYI
+Ezután a végpontok elérhetőek a http://localhost:8080/api/ url-en. Illetve a gyökérmappán található egy postman collection (*.../CAFF-API [szbizt].postman_collection.json*), amiben mintha request-ek találhatóak.
--- catalogbackend/src/main/java/hu/bme/sfg/catalogbackend/CatalogBackendApplication.java
+++ catalogbackend/src/main/java/hu/bme/sfg/catalogbackend/CatalogBackendApplication.java
@@ -1,13 +1,88 @@
 package hu.bme.sfg.catalogbackend;
 
+import hu.bme.sfg.catalogbackend.domain.Comment;
+import hu.bme.sfg.catalogbackend.domain.Picture;
+import hu.bme.sfg.catalogbackend.domain.User;
+import hu.bme.sfg.catalogbackend.repository.CommentRepository;
+import hu.bme.sfg.catalogbackend.repository.PictureRepositroy;
+import hu.bme.sfg.catalogbackend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CatalogBackendApplication {
+@Slf4j
+@AllArgsConstructor
+public class CatalogBackendApplication implements CommandLineRunner {
+
+    private UserRepository userRepository;
+
+    private PictureRepositroy pictureRepositroy;
+
+    private CommentRepository commentRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CatalogBackendApplication.class, args);
+        log.info("CatalogBackendApplication is successfully started");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //Test data
+        User userAnne = User.builder()
+                .email("anne@email.hu")
+                .userName("anne")
+                .build();
+        User userPeter = User.builder()
+                .email("peter@email.hu")
+                .userName("peter")
+                .build();
+
+        userRepository.saveAll(Arrays.asList(userAnne, userPeter));
+
+        Picture pictureFlower = Picture.builder()
+                .name("Lila virágok")
+                .content(null)
+                .user(userAnne)
+                .build();
+        Picture pictureDog = Picture.builder()
+                .name("Egy shiba inu kutya")
+                .content(null)
+                .user(userAnne)
+                .build();
+        Picture pictureSky = Picture.builder()
+                .name("Kék ég")
+                .content(null)
+                .user(userPeter)
+                .build();
+        Picture pictureGrass = Picture.builder()
+                .name("Zöld fű")
+                .content(null)
+                .build();
+
+        pictureRepositroy.saveAll(Arrays.asList(pictureFlower, pictureDog, pictureSky, pictureGrass));
+
+        Comment commentDog = Comment.builder()
+                .picture(pictureDog)
+                .comment("Nagyon aranyos")
+                .user(userPeter)
+                .build();
+        Comment commentSky1 = Comment.builder()
+                .picture(pictureSky)
+                .comment("Jó kék az ég")
+                .user(userAnne)
+                .build();
+        Comment commentSky2 = Comment.builder()
+                .picture(pictureSky)
+                .comment("Ugye, szerintem is nagyon szép")
+                .user(userPeter)
+                .build();
+
+        commentRepository.saveAll(Arrays.asList(commentDog, commentSky1, commentSky2));
     }
 
 }
